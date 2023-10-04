6a353b099e7de27d8552fc2d659497c0ef289fd0
bgrey1 <124700028+bgrey1@users.noreply.github.com>
2023-10-04 10:45:08 +0100

    nested for loop done

---
+++ loopInFives.php
@@ -0,0 +1,15 @@
+<?php
+#phpForLoop - Brandon Grey week3 lab /program here -want it to count to 20
+#Brandon Grey 04/10/23
+#a loop that counts to 20 - 
+#this program uses a loop to count up to 20 and puts the value of the loop counter out to the screen on each iteration
+for ($i=1;$i<=20;$i++) {
+    echo "$i, ";
+    if($i%5==0)
+    {
+        echo"<br>";
+    }
+}
+
+?>
+
---
+++ nestedForLoop.php
@@ -0,0 +1,15 @@
+<?php
+echo "<TABLE border ='1'>";
+
+for ($i=1;$i<=20;$i++) {
+    echo "<TR>";
+
+for ($j=1; $j<=15; $j++) {
+    echo "<TD>row$j, col$i</TD>";
+    if($j%15==0);
+}
+        echo"</TR>";
+    }
+echo"</TABLE>";
+?>
+
