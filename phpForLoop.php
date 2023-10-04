68130d8281caf89bd3a67dc4955fc0cafe2b8265
bgrey1 <124700028+bgrey1@users.noreply.github.com>
2023-10-04 09:36:52 +0100

    counts to 20

--- phpForLoop.php
+++ phpForLoop.php
@@ -1,9 +1,9 @@
 <?php
-#phpForLoop - put the name of the file/program here - notes the hashtag indicates a comment in PHP
-#put your name and the date here
-#put a descripition of what the code does here - 
-#this program uses a loop to count up to 10 and puts the value of the loop counter out to the screen on each iteration
-for ($i=1;$i<=10;$i++) {
+#phpForLoop - Brandon Grey week3 lab /program here -want it to count to 20
+#Brandon Grey 04/10/23
+#a loop that counts to 20 - 
+#this program uses a loop to count up to 20 and puts the value of the loop counter out to the screen on each iteration
+for ($i=1;$i<=20;$i++) {
     echo "$i, ";
 }
 
